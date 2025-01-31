934873510e9c81ae58ec4a5233620830642bfc09
Victor Cano <vcanoramos83@gmail.com>
2025-01-31 12:55:05 +0100

    Codigo de adivina numero implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Hola Mundo</p>
+      <header>
+        <h1>Adivina El Número Subnormal!</h1>
+        <p className="between">(Between 1 and 20)</p>
+        <button className="btn again">Otra Vez!</button>
+        <div className="number">?</div>
+      </header>
+      <main>
+        <section className="left">
+          <input type="number" className="guess" />
+          <button className="btn check">Check!</button>
+        </section>
+        <section className="right">
+          <p className="message">Dale caña...</p>
+          <p className="label-score">
+            💯 Score: <span className="score">20</span>
+          </p>
+          <p className="label-highscore">
+            🥇 Highscore: <span className="highscore">0</span>
+          </p>
+        </section>
+      </main>
+      <script src="script.js"></script>
     </>
   );
 }
